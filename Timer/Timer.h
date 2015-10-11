3fd30954815da83cb5fd15e8e98223517cc3ae97
Wallace Campos <wamacam@gmail.com>
2015-10-11 11:47:57 -0300

    Initial Commit

---
+++ Timer/Timer.h
@@ -0,0 +1,19 @@
+//
+//  Timer.h
+//  Timer
+//
+//  Created by Wallace Maia Campos on 11/10/15.
+//  Copyright © 2015 Wallace Maia Campos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Timer.
+FOUNDATION_EXPORT double TimerVersionNumber;
+
+//! Project version string for Timer.
+FOUNDATION_EXPORT const unsigned char TimerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Timer/PublicHeader.h>
+
+
